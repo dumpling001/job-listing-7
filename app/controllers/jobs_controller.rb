9ce81bfa603a9c073523d40d17f9e3e37de8bc5c
dumpling001 <dumpling001@gmail.com>
2017-04-19 15:47:20 +0800

    change image of welcome page/some text/logo.png, add search function

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,14 +2,8 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-      @jobs = case params[:order]
-              when 'by_lower_bound'
-                Job.published.order('wage_lower_bound DESC')
-              when 'by_upper_bound'
-                Job.published.order('wage_upper_bound DESC')
-              else
-                Job.published.recent
-              end
+    @search = Job.ransack(params[:q])
+    @jobs = @search.result.where(is_hidden: false)
   end
 
   def show
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @search = Job.ransack(params[:q])
+    @jobs = @search.result.where(is_hidden: false)
   end
 end
