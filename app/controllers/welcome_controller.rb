39674ed5ad800b9aca25d99a0597e5d5514f5c80
dumpling001 <dumpling001@gmail.com>
2017-04-25 16:42:06 +0800

    local workplace_id is different from heroku. local1234:北京深圳上海杭州，hheroku1234:北京杭州深圳上海

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,13 @@ class WelcomeController < ApplicationController
   def index
     @search = Job.ransack(params[:q])
     @jobs = @search.result.where(is_hidden: false)
+    # @bj = Job.where(workplace_id:'1') local
+    # @sz = Job.where(workplace_id:'2') local
+    # @sh = Job.where(workplace_id:'3') local
+    # @hz = Job.where(workplace_id:'4') local
     @bj = Job.where(workplace_id:'1')
-    @sz = Job.where(workplace_id:'2')
-    @sh = Job.where(workplace_id:'3')
-    @hz = Job.where(workplace_id:'4')
+    @sz = Job.where(workplace_id:'3')
+    @sh = Job.where(workplace_id:'4')
+    @hz = Job.where(workplace_id:'2')
   end
 end
