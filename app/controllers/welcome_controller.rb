83237d2bbb62992c259bf3cb6e29e19130d3d540
dumpling001 <dumpling001@gmail.com>
2017-04-26 18:43:32 +0800

    delete pagenate button

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @search = Job.ransack(params[:q])
     @jobs = @search.result.published
-    # @bj = Job.where(workplace_id:'1') local
-    # @sz = Job.where(workplace_id:'2') local
-    # @sh = Job.where(workplace_id:'3') local
-    # @hz = Job.where(workplace_id:'4') local
     @bj = Job.where(workplace_id:'1').order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
     @sz = Job.where(workplace_id:'3').order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
     @sh = Job.where(workplace_id:'4').order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
