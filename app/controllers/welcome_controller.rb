8b7d834bd939929389481e246e5d161d9604db34
dumpling001 <dumpling001@gmail.com>
2017-04-26 10:44:13 +0800

    city jobs display in order('wage_lower_bound DESC')

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,14 @@
 class WelcomeController < ApplicationController
   def index
     @search = Job.ransack(params[:q])
-    @jobs = @search.result.where(is_hidden: false)
+    @jobs = @search.result.published
     # @bj = Job.where(workplace_id:'1') local
     # @sz = Job.where(workplace_id:'2') local
     # @sh = Job.where(workplace_id:'3') local
     # @hz = Job.where(workplace_id:'4') local
-    @bj = Job.where(workplace_id:'1')
-    @sz = Job.where(workplace_id:'3')
-    @sh = Job.where(workplace_id:'4')
-    @hz = Job.where(workplace_id:'2')
+    @bj = Job.where(workplace_id:'1').order('wage_lower_bound DESC')
+    @sz = Job.where(workplace_id:'3').order('wage_lower_bound DESC')
+    @sh = Job.where(workplace_id:'4').order('wage_lower_bound DESC')
+    @hz = Job.where(workplace_id:'2').order('wage_lower_bound DESC')
   end
 end
