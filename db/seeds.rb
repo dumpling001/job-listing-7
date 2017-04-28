f6e25ec6a71efb8ab531f898dc59b0b382301601
dumpling001 <dumpling001@gmail.com>
2017-04-28 10:47:13 +0800

    change seed file to fix NoMethodError

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ puts "这个seeds文件可以自动建立一个admin账号，并且创建20个pu
 
 create_account = User.create([email: 'example@gmail.com', password:'123456', password_confirmation: '123456', is_admin: 'true'])
 puts "Admin account created."
-
+@user = User.find_by(:email => "example@gmail.com")
 create_job = for i in 1..20 do
-Job.create!([title: "Job no.#{i}", description: "这是用seeds文件建立的第 #{i} 个公开的工作", wage_upper_bound: rand(20..50)*1000, wage_lower_bound: rand(10..20)*1000, is_hidden: "false", workplace_id: rand(1..4)])
+Job.create!([title: "Job no.#{i}", description: "这是用seeds文件建立的第 #{i} 个公开的工作", :user => @user, wage_upper_bound: rand(20..50)*1000, wage_lower_bound: rand(10..20)*1000, is_hidden: "false", workplace_id: rand(1..4)])
 end
 create_job = for i in 1..20 do
-Job.create!([title: "Job no.#{i}", description: "这是用seeds文件建立的第 #{i} 个隐藏的工作", wage_upper_bound: rand(20..50)*1000, wage_lower_bound: rand(5..15)*1000, is_hidden: "true", workplace_id: rand(1..4)])
+Job.create!([title: "Job no.#{i}", description: "这是用seeds文件建立的第 #{i} 个隐藏的工作", :user => @user, wage_upper_bound: rand(20..50)*1000, wage_lower_bound: rand(5..15)*1000, is_hidden: "true", workplace_id: rand(1..4)])
 end
 
 puts "20 Hidden jobs created."
