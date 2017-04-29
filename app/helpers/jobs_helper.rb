7595009f11f9bef9853aaef778f639f96711c239
dumpling001 <dumpling001@gmail.com>
2017-04-29 15:49:56 +0800

    add render_member_status and render_voter_status

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -13,4 +13,21 @@ module JobsHelper
       highlight(excerpt_cont, query_string)
   end
 
+  def render_member_status(job)
+    if current_user && current_user.is_member_of?(job)
+          link_to("", outbox_job_path(job), method: :post, class: "fa fa-folder")
+    else
+          link_to("", inbox_job_path(job), method: :post, class: "fa fa-folder-o")
+    end
+  end
+
+
+  def render_voter_status(job)
+     if current_user && current_user.is_voter_of?(job)
+       link_to("", downvote_job_path(job), method: :post, class: "fa fa-heart", style: "color:red;")
+         else
+       link_to("", upvote_job_path(job), method: :post, class: "fa fa-heart-o")
+     end
+  end
+
 end
