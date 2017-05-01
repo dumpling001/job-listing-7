336009590adb215fb87a17304d573b998ea65b45
dumpling001 <dumpling001@gmail.com>
2017-05-01 12:04:40 +0800

    @city_items.each do |city| to <%= render :partial => city_item, :collection => @xxx, :as => :job %>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
   def index
     @search = Job.ransack(params[:q])
     @jobs = @search.result.published
+
+    @hit = Job.published.order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
+
     @bj = Job.where(workplace_id:'1').order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
     @sz = Job.where(workplace_id:'2').order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
     @sh = Job.where(workplace_id:'3').order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
